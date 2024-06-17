a53c31b14b19a99be6c59b1de4f000723b2919aa
cyrusbuffington <cyrusbuffington@gmail.com>
2024-06-17 14:04:52 -0700

    refactored popup.js

--- popup.js
+++ popup.js
@@ -1,47 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let n = 12;
+    initializeCanvas(6);
+});
 
+function initializeCanvas(n) {
     let canvas = document.getElementById('canvas');
     let ctx = canvas.getContext('2d');
 
-    canvas.height = 600;
-    canvas.width = 600;
+    canvas.height = n * 100;
+    canvas.width = n * 100;
+
+    fillCanvasWithColor(ctx, "#FFFFFF");
 
     let canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
     canvas.addEventListener('click', function(event) {
-        // Calculate the clicked pixel's coordinates
-        let rect = canvas.getBoundingClientRect();
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
-
-        x = Math.floor(x / n) * n;
-        y = Math.floor(y / n) * n;
-
-        // Color the clicked pixel black
-        ctx.fillStyle = "#000000";
-        ctx.fillRect(x, y, n, n);
-
+        let coordinates = calculatePixelCoordinates(event, n);
+        fillPixelWithColor(ctx, coordinates, n, "#000000");
         canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     });
 
     canvas.addEventListener('mousemove', function(event) {
         ctx.putImageData(canvasData, 0, 0);
-
-        // Calculate the hovered pixel's coordinates
-        let rect = canvas.getBoundingClientRect();
-        let x = event.clientX - rect.left;
-        let y = event.clientY - rect.top;
-
-        x = Math.floor(x / n) * n;
-        y = Math.floor(y / n) * n;
-
-        // Draw a border around the hovered pixel
-        ctx.strokeStyle = "#000000";
-        ctx.strokeRect(x, y, n, n);
+        let coordinates = calculatePixelCoordinates(event, n);
+        outlinePixel(ctx, coordinates, n, "#000000");
     });
+}
 
-})
+function fillCanvasWithColor(ctx, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function calculatePixelCoordinates(event, n) {
+    let rect = event.target.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    x = Math.floor(x / n) * n;
+    y = Math.floor(y / n) * n;
+
+    return {x, y};
+}
+
+function fillPixelWithColor(ctx, coordinates, n, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(coordinates.x, coordinates.y, n, n);
+}
+
+function outlinePixel(ctx, coordinates, n, color) {
+    ctx.strokeStyle = color;
+    ctx.strokeRect(coordinates.x, coordinates.y, n, n);
+}
