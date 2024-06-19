37efd971abb44b5c3be702dcaf379fc683d57c75
cyrusbuffington <cyrusbuffington@gmail.com>
2024-06-19 16:38:30 -0700

    adding buggy panning. need to fix

---
+++ canvas.js
@@ -0,0 +1,192 @@
+class Canvas {
+    constructor(n, length) {
+        this.canvas = document.getElementById('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.zoom = 1;
+        this.n = n;
+        this.length = length;
+        this.canvasData = null; //This will be loaded in from server
+        this.placeEnabled= true;
+
+        this.outlineCanvas = document.getElementById('outlineCanvas');
+        this.outlineCtx = this.outlineCanvas.getContext('2d');
+
+        this.isDragging = false;
+        this.startX = 0;
+        this.startY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+
+        // Bind the event handlers to 'this'
+        this.onMouseDown = this.onMouseDown.bind(this);
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onMouseUp = this.onMouseUp.bind(this);
+
+        // Add the event listeners
+        this.canvas.addEventListener('mousedown', this.onMouseDown);
+        this.canvas.addEventListener('mousemove', this.onMouseMove);
+        this.canvas.addEventListener('mouseup', this.onMouseUp);
+        
+
+
+    }
+
+    initializeCanvas() {
+        this.ctx.imageSmoothingEnabled = false;
+        this.ctx.willReadFrequently = true;
+
+        this.canvas.height = this.n * this.length;
+        this.canvas.width = this.n * this.length;
+
+        this.fillCanvasWithColor("#FFFFFF");
+        this.saveData();
+
+    }
+
+    initializeOutline() {
+        this.outlineCanvas.height = this.n * this.length;
+        this.outlineCanvas.width = this.n * this.length;
+        this.outlineCtx.clearRect(0, 0, this.outlineCanvas.width, this.outlineCanvas.height);
+        this.outlineCtx.fillStyle = "#000000";
+        for (let i = 0; i < this.canvas.width; i += this.n) {
+            this.outlineCtx.fillRect(i, 0, 1 / this.zoom, this.canvas.height);
+        }
+        for (let i = 0; i < this.canvas.height; i += this.n) {
+            this.outlineCtx.fillRect(0, i, this.canvas.width, 1 / this.zoom);
+        }
+
+        this.outlineCanvas.classList.add('hide');
+    }
+
+    saveData() {
+        this.canvasData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+
+    fillCanvasWithColor(color) {
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
+
+    drawPixel(event, color) {
+        if (this.placeEnabled) {
+            this.ctx.fillStyle = color;
+            let coordinates = this.calculatePixelCoordinates(event);
+            this.ctx.fillRect(coordinates.x, coordinates.y, this.n, this.n);
+        }
+    }
+
+    hoverPixel(event) {
+        this.ctx.putImageData(this.canvasData, 0, 0);
+        this.ctx.globalCompositeOperation = 'multiply'; // Set the composite operation to 'multiply'
+        this.drawPixel(event, "rgba(0, 0, 0, 0.5)"); // Fill the pixel with a semi-transparent black color
+        this.ctx.globalCompositeOperation = 'source-over'; // Reset the composite operation to 'source-over'
+
+    }
+
+    calculatePixelCoordinates(event) {
+        let rect = event.target.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+
+        x = Math.floor(x / this.n / this.zoom) * this.n;
+        y = Math.floor(y / this.n / this.zoom) * this.n;
+
+        return {x, y};
+    }
+
+    calculateRealCoordinates(event) {
+        let rect = this.canvas.getBoundingClientRect(); 
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        return {x, y};
+    }
+
+    zoomIn(event) {
+
+        event.preventDefault();
+        let coordinates = this.calculateRealCoordinates(event);
+
+        if (this.zoom < 4) {
+            this.placeEnabled = false;
+
+            this.canvas.style.transformOrigin = `${coordinates.x / this.zoom}px ${coordinates.y / this.zoom}px`;
+            this.outlineCanvas.style.transformOrigin = `${coordinates.x / this.zoom}px ${coordinates.y / this.zoom}px`;
+
+            this.zoom *= 2;
+
+            this.canvas.style.transform = `scale(${this.zoom})`;
+            this.outlineCanvas.style.transform = `scale(${this.zoom})`;
+        }
+
+        if (this.zoom == 4){
+            this.outlineCanvas.classList.remove('hide');
+        }
+        setTimeout(() => {
+            this.placeEnabled = true;
+        }, 100);
+
+        
+    }
+
+    zoomOut(event) {
+
+        event.preventDefault();
+        let coordinates = this.calculateRealCoordinates(event);
+
+        if (this.zoom > 1) {
+            this.placeEnabled = false;
+
+            this.canvas.style.transformOrigin = `${coordinates.x / this.zoom}px ${coordinates.y / this.zoom}px`;
+            this.outlineCanvas.style.transformOrigin = `${coordinates.x / this.zoom}px ${coordinates.y / this.zoom}px`;
+
+            this.zoom/= 2;
+
+            this.canvas.style.transform = `scale(${this.zoom})`;
+            this.outlineCanvas.style.transform = `scale(${this.zoom})`;
+        }
+        if (this.zoom != 4){
+            this.outlineCanvas.classList.add('hide');
+        }
+        setTimeout(() => {
+            this.placeEnabled = true;
+        }, 100);
+        
+    }
+
+    onMouseDown(event) {
+        this.isDragging = true;
+        this.startX = event.clientX - this.offsetX;
+        this.startY = event.clientY - this.offsetY;
+    }
+
+    onMouseMove(event) {
+        if (!this.isDragging) return;
+        
+        this.placeEnabled = false;
+        if (this.zoom == 1) return;
+
+        this.offsetX = event.clientX - this.startX;
+        this.offsetY = event.clientY - this.startY;
+
+        this.updateCanvasPosition();
+    }
+
+    onMouseUp() {
+        this.isDragging = false;
+        setTimeout(() => {
+            this.placeEnabled = true;
+        }, 100);
+        
+    }
+    updateCanvasPosition() {
+        this.canvas.style.top = this.offsetY + 'px';
+        this.canvas.style.left = this.offsetX + 'px';
+        this.outlineCanvas.style.top = this.offsetY + 'px';
+        this.outlineCanvas.style.left = this.offsetX + 'px';
+    }
+
+}
+
+export { Canvas };
+
--- popup.js
+++ popup.js
@@ -1,53 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    initializeCanvas(6);
+import {Canvas} from './canvas.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let board = new Canvas(10, 50);
+    board.initializeCanvas();
+    board.initializeOutline();
+
+    board.canvas.addEventListener('click', (event) => {
+        board.drawPixel(event, "#FF0000");
+        board.saveData();
+
+    });
+    board.canvas.addEventListener('mousemove', (event) => {
+        board.hoverPixel(event, "#000000");
+    });
+
+    board.canvas.addEventListener('wheel', (event) => {
+        if (event.deltaY < 0) {
+            board.zoomIn(event);
+        } else {
+            board.zoomOut(event);
+        }
+    });
 });
 
-function initializeCanvas(n) {
-    let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
-
-    canvas.height = n * 100;
-    canvas.width = n * 100;
-
-    fillCanvasWithColor(ctx, "#FFFFFF");
-
-    let canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-    canvas.addEventListener('click', function(event) {
-        let coordinates = calculatePixelCoordinates(event, n);
-        fillPixelWithColor(ctx, coordinates, n, "#000000");
-        canvasData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    });
-
-    canvas.addEventListener('mousemove', function(event) {
-        ctx.putImageData(canvasData, 0, 0);
-        let coordinates = calculatePixelCoordinates(event, n);
-        outlinePixel(ctx, coordinates, n, "#000000");
-    });
-}
-
-function fillCanvasWithColor(ctx, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-}
-
-function calculatePixelCoordinates(event, n) {
-    let rect = event.target.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-
-    x = Math.floor(x / n) * n;
-    y = Math.floor(y / n) * n;
-
-    return {x, y};
-}
-
-function fillPixelWithColor(ctx, coordinates, n, color) {
-    ctx.fillStyle = color;
-    ctx.fillRect(coordinates.x, coordinates.y, n, n);
-}
-
-function outlinePixel(ctx, coordinates, n, color) {
-    ctx.strokeStyle = color;
-    ctx.strokeRect(coordinates.x, coordinates.y, n, n);
-}
